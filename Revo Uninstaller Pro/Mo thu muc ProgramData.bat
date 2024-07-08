e397a6c7f5fcb94ce22f068f0bb35f5a4af1f855
dna <ducnguyen6693@gmail.com>
2024-07-08 15:50:18 +0700

    a

---
+++ Revo Uninstaller Pro/Mo thu muc ProgramData.bat
@@ -0,0 +1,3 @@
+@echo off
+@start "" "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro"
+@XCOPY "D:\Software\RevoUninstallerPro\Revo Uninstaller Pro\Revo Uninstaller Pro" "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro" /Y
